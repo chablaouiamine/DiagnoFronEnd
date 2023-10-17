ae0516bda374ac8e830c9a78acd0ddb1e6cce749
chablaouiamine <140975778+chablaouiamine@users.noreply.github.com>
2023-10-17 16:28:22 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './components/Login/Login'
-import Header from './components/Header/Header'
-import Quiz from './components/Quiz/Quiz'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Header from "./components/Header/Header";
+import Quiz from "./components/Quiz/Quiz";
+import Register from "./components/Register/Register";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       {/* <Header /> */}
       {/* <Login /> */}
-      <Quiz />
+      <Register />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,92 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Register() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [registrationMessage, setRegistrationMessage] = useState("");
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const { username, email, password } = formData;
+
+    // Replace this URL with your registration API endpoint
+    const apiUrl = "http://localhost:8092/users/add";
+
+    axios
+      .post(apiUrl, { username, email, password })
+      .then((response) => {
+        setRegistrationMessage(response.data.message);
+      })
+      .catch((error) => {
+        setRegistrationMessage("Registration failed. Please try again.");
+        console.error("Registration failed:", error);
+      });
+  };
+
   return (
-    <div>Register</div>
-  )
+    <div>
+      <form
+        action=""
+        className="d-flex flex-column"
+        onSubmit={handleFormSubmit}
+      >
+        <div className="col-4 mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            className="form-control"
+            id="exampleInputUsername"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="col-4 mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="form-control"
+            id="exampleInputEmail"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <div className="col-4 mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary col-4">
+          Submit
+        </button>
+      </form>
+      {registrationMessage && <p>{registrationMessage}</p>}
+    </div>
+  );
 }
