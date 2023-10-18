39579451e6d00e229a66901760953e1947f6c675
chablaouiamine <140975778+chablaouiamine@users.noreply.github.com>
2023-10-18 01:04:45 +0100

    fifth commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,36 +1,76 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
-  return (
-    <div>
-      <form action="" className="d-flex flex-column">
-        <div className="col-4 mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-        </div>
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-        <div className="col-4 mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-       
-        <button type="submit" className="btn btn-primary col-4">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+  const [loginMessage, setLoginMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = formData;
+
+    // Replace this URL with your login API endpoint
+    const apiUrl = "http://localhost:8092/users/login";
+
+    axios
+      .post(apiUrl, { email, password })
+      .then((response) => {
+        setLoginMessage(response.data.message);
+        // You can add logic to handle successful login, e.g., redirect to a dashboard.
+      })
+      .catch((error) => {
+        setLoginMessage("Login failed. Please check your email and password.");
+        console.error("Login failed:", error);
+      });
+    return (
+      <div>
+        <form
+          action=""
+          className="d-flex flex-column"
+          onSubmit={handleFormSubmit}
+        >
+          <div className="col-4 mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+
+          <div className="col-4 mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary col-4">
+            Submit
+          </button>
+        </form>
+        {loginMessage && <p>{loginMessage}</p>}
+      </div>
+    );
+  };
 }
