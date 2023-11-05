797773fb8cfc25b6042167fbf448359ae55c5a69
chablaouiamine <140975778+chablaouiamine@users.noreply.github.com>
2023-11-06 00:39:56 +0100

    modified quiz component

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import './quiz.css';
 
-export default function Quiz({ onSubmit }) {
+export default function Quiz() {
+  const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
   const [choices, setChoices] = useState([]);
   const [response, setResponse] = useState([]);
@@ -10,6 +13,8 @@ export default function Quiz({ onSubmit }) {
     questionId: "",
     userId: "1",
   });
+  const [buttonToDisplay, setButtonToDisplay] = useState("Submit");
+
   const categoriesListAPI = "http://localhost:8092/categories/list";
   const choicesListAPI = "http://localhost:8092/choices/list";
   const postAPI = "http://localhost:8092/Responses/add";
@@ -50,12 +55,19 @@ export default function Quiz({ onSubmit }) {
       setCurrentCategoryIndex(currentCategoryIndex + 1);
       setCurrentQuestionIndex(0);
     } else {
-      onSubmit();
+      handleFormSubmit(); // Submit the form internally
     }
+    setButtonToDisplay("Submit");
+    // setFormSubmitted(false);
+    // setShowSubmitButton(true);
   };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  const clearChoiceId = () => {
+    setFormData({ ...formData, choiceId: "" });
+  };
+
+
+  const handleFormSubmit = async () => {
     try {
       const { choiceId } = formData;
       const questionId = currentQuestion?.id.toString();
@@ -66,35 +78,72 @@ export default function Quiz({ onSubmit }) {
         userId,
       });
       setResponse(response.data);
+      setButtonToDisplay("Next");
+      // setFormSubmitted(true);
+      // setShowSubmitButton(false);
+      if (currentCategoryIndex === categories.length - 1 && currentQuestionIndex === currentCategory.quest.length - 1) {
+        // If this is the last question and category, navigate to another page
+        navigate('/'); // Replace '/another-page' with the URL you want to navigate to
+      }
     } catch (error) {
       console.error("Error submitting form: ", error);
     }
   };
 
   return (
-    <div>
-      <h1>{currentCategory?.libelle}</h1>
-      <p>{currentQuestion?.description}</p>
-      {choices.map((choice) => (
-        <div className="form-check" key={choice.id}>
-          <label className="form-check-label">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="choiceId"
-              value={choice.id}
-              onChange={handleInputChange}
-            />
-            {choice.description}
-          </label>
+    <>
+      <div className="d-flex justify-content-start">
+        <h1>{currentCategory?.libelle}</h1>
+      </div>
+      <div className="container d-flex flex-column align-items-center justify-content-center">
+        <h2>{currentQuestion?.description}</h2>
+        <div className="d-flex flex-column mb-3 radio-list">
+          {choices.map((choice) => (
+            <div
+              // className="form-check radio-item" 
+              className="radio-item" 
+              key={choice.id}>
+
+              <input
+                // className="form-check-input"
+                type="radio"
+                name="choiceId"
+                value={choice.id}
+                onChange={handleInputChange}
+                id={choice.id}
+              />
+              <label
+                // className="form-check-label"
+                htmlFor={choice.id}
+                style={{ fontSize: "18px", fontWeight: "500" }}
+              >
+                {choice.description}
+              </label>
+            </div>
+          ))}
         </div>
-      ))}
-      <button type="button" onClick={handleNextQuestion}>
-        Next
-      </button>
-      <button type="submit" onClick={handleFormSubmit}>
-        Submit
-      </button>
-    </div>
+        <div className="d-flex space-between ">
+          {buttonToDisplay == "Next" ? (
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={handleNextQuestion}
+            >
+              Next
+            </button>
+          ) : null}
+          {buttonToDisplay == "Submit" ? (
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={handleFormSubmit}
+              style={{ marginLeft: "3px" }}
+            >
+              Submit
+            </button>
+          ) : null}
+        </div>
+      </div>
+    </>
   );
 }
